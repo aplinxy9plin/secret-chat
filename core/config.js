36dcf29e3247ebb22a5c362c369d46ec65a46b2a
codemekh@gmail.com <codemekh@gmail.com>
2020-03-23 00:20:34 +0700

    Поменял базу и secretKey

--- core/config.js
+++ core/config.js
@@ -3,7 +3,7 @@ import path from 'path';
 export default {
   logFileDir: path.join(__dirname, '../log'),
   logFileName: 'app.log',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/secret123',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://admin:q2w3e4r5@ds127736.mlab.com:27736/heroku_gtqj7p03',
   serverPort: process.env.PORT || 1335,
-  secretKey: process.env.secretKey || 'G9AiXcKQNhIZb9mZm9cZ',
+  secretKey: process.env.secretKey || 'qe2s2jHn0RF9I6CQhCYK',
 };
