ad7e44d84d658075593dd04372c96675c0a08758
codemekh@gmail.com <codemekh@gmail.com>
2020-03-28 11:53:44 +0700

    Изменил тип vk user ID в модели чат архива. Теперь здесь тоже NUMBER

--- models/chatArchive.model.js
+++ models/chatArchive.model.js
@@ -4,7 +4,7 @@ const ChatArchiveSchema = mongoose.Schema({
   messages: [{
     type: { type: String, enum: ['text', 'document'], required: true },
     text: { type: String },
-    sender: { type: String, enum: ['1', '2'], required: true },
+    sender: { type: Number, enum: ['1', '2'], required: true },
     date: { type: Date, required: true },
     documents: { type: mongoose.Schema.Types.ObjectId, ref: 'documents' },
   }],
